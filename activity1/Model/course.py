768ddde620606737932864ad2b70e4d633853d1c
AzanJaved88 <62589563+AzanJaved88@users.noreply.github.com>
2020-03-24 00:43:46 -0700

    firstcommit

---
+++ activity1/Model/course.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class COURSE:
+    def __init__(self):
+        self.c_id = None
+        self.c_name = None
+        self.c_teacher = None
+
+    def updatelecture(self, ):
+        pass
+
+    def enrollstudent(self, ):
+        pass
+
---
+++ activity1/Model/doctor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class doctor:
+    def __init__(self):
+        self.d_id = None
+        self.d_name = None
+        self.location = None
+        self.d_speciality = None
+
+    def chargefees(self, ):
+        pass
+
+    def appoitment(self, ):
+        pass
+
